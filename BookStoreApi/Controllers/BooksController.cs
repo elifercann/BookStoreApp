5def42a3a64fce5f2a08df26fb1cb338fd2e3559
elifercann <ercanelif450@gmail.com>
2023-05-08 20:57:29 +0300

    Add project files.

---
+++ BookStoreApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using BookStoreApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+        public BooksController(ApplicationContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books=_context.Books.ToList();
+            return Ok(books);
+
+        }
+    }
+}
---
+++ BookStoreApi/Repositories/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Repositories.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Repositories
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions options):base(options)
+        { 
+
+        }
+       
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }
+}
