c33ab2fecc18582b8dc626dc230019f90fe4456e
elifercann <ercanelif450@gmail.com>
2023-05-08 21:57:17 +0300

    http metotları yazıldı test edildi

--- BookStoreApi/Controllers/BooksController.cs
+++ BookStoreApi/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using BookStoreApi.Repositories;
+﻿using BookStoreApi.Models;
+using BookStoreApi.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +13,62 @@ namespace BookStoreApi.Controllers
         public BooksController(ApplicationContext context)
         {
             _context = context;
-            
+
         }
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            var books=_context.Books.ToList();
-            return Ok(books);
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
 
+                throw new Exception(ex.Message);
+            }
+
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOneBook([FromRoute] int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(x => x.Id == id).SingleOrDefault();
+                if (book == null)
+                    return NotFound();
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+
+            }
+
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book == null)
+                    return BadRequest();
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+           
         }
     }
 }
